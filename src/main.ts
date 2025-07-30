293130acd2ac18e87d94831ff4568bfe36e7fc8b
OskarEriksen <omle.eriksen@gmail.com>
2025-07-30 10:40:25 +0200

    changing port number

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3001',
+    credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
   try {
     await datasource.initialize();
     console.log('✅ DB Connection successful!');
