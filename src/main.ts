37d5ea7ed3911044181f1d3345cce8b59ac40ba3
OskarEriksen <omle.eriksen@gmail.com>
2025-07-30 10:51:20 +0200

    Added frontend url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import datasource from 'data.source';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://player-data-frontend.vercel.app/',
+    ],
     credentials: true,
   });
 
